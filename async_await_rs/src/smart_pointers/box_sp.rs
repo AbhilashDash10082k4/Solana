566372a8cffd439eae1a416230ef010b3dae3378
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-09-04 22:43:59 +0530

    drop trait

--- async_await_rs/src/smart_pointers/box_sp.rs
+++ async_await_rs/src/smart_pointers/box_sp.rs
@@ -3,7 +3,7 @@
     - Box - returns a pointer to List which is of definite size independent of List size
     - impls Deref -> allows the vals to be used as references and impls Drop -> removes the var after it goes out of scope
 */
-use std::ops::Deref;
+use std::{ops::Deref, mem::drop};
 
 #[derive(Debug)]
 pub enum List {
@@ -12,17 +12,23 @@ pub enum List {
 }
 
 //custom smart pointer
-pub struct Custom_SP<T>(T);
-impl<T> Custom_SP<T> {
-    fn new(x: T) -> Custom_SP<T> {
-        Custom_SP(x)
+pub struct CustomSP<T>(T);
+impl<T> CustomSP<T> {
+    fn new(x: T) -> CustomSP<T> {
+        CustomSP(x)
     }
 }
-impl<T> Deref for Custom_SP<T> {
+impl<T> Deref for CustomSP<T> {
     type Target = T;
     /* .0 returns the 1st elem of th etuple struct*/
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+impl<T> Drop for CustomSP<T> {
+    fn drop (&mut self) {
+        println!("CustomSp dropped");
+        //will run when the struct will be dropped
     }
 }
 pub fn smart_pointers() {
@@ -33,9 +39,9 @@ pub fn smart_pointers() {
     
     let x = 5;
     let y = Box::new(x);
-    let z = Custom_SP::new(x);
+    let z = CustomSP::new(x);
     /*y has copied value of x in Box rather than having a reference to x,
-    z = uses Custom_SP which returns a reference to inner val on dereferencing
+    z = uses CustomSP which returns a reference to inner val on dereferencing
     */
     assert_eq!(5, *y);
     assert_eq!(5, x);
@@ -43,16 +49,17 @@ pub fn smart_pointers() {
 
     /*Deref coercion - 
     Rust calls deref again to convert &String to &str which is the param type of hello
-    1st call -> &Custom_SP<Stirng> to &String
+    1st call -> &CustomSP<Stirng> to &String
     2nd call -> &String to &str
 
     Rust can call Dref::deref any no. of times to convert the type of data to match param type. This (the no. of calls) is decided at compile time so there is no load on runtime
 
     Deref trait can be also used with mutable vals. The immutable vals cannot be converted to mutable references but the reverse can happen
     */
+    drop(x); //drops the data passed before going out of scope
     fn hello(s: &str) {
         println!("{s}");
     }
-    let s = Custom_SP::new(String::from("ad"));
+    let s = CustomSP::new(String::from("ad"));
     hello(&s);
 }
